c86b0aca41a069ad1e6ad08dddc93892262ba223
eharpste <eharpste@cs.cmu.edu>
2017-02-21 14:54:53 -0500

    Updated the MultiTag system to provide Component short cut methods

--- Unity Project/Assets/Scripts/MultiTag/MultiTag.cs
+++ Unity Project/Assets/Scripts/MultiTag/MultiTag.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 
 [ExecuteInEditMode()]
@@ -7,11 +8,11 @@ public class MultiTag : MonoBehaviour {
 
     #region ================ Static Properties ======================
 
-    private static Dictionary<string, List<GameObject>> tagMap = new Dictionary<string,List<GameObject>>();
+    private static Dictionary<string, HashSet<GameObject>> tagMap = new Dictionary<string, HashSet<GameObject>>();
 
     private static bool AddToMap(string tag, GameObject go) {
         if (!tagMap.ContainsKey(tag)) {
-            tagMap[tag] = new List<GameObject>();
+            tagMap[tag] = new HashSet<GameObject>();
         }
         if (!tagMap[tag].Contains(go)) {
             tagMap[tag].Add(go);
@@ -42,6 +43,24 @@ public class MultiTag : MonoBehaviour {
         return FindOneGameObjectWithTag(tag);
     }
 
+
+    /// <summary>
+    /// Find a single Component on a GameObject with the given tag. Designed to mimic the standard
+    /// tag API but just a wrapper for calling FindOneComponentWithTag().
+    /// </summary>
+    /// <typeparam name="T">Some Component Type</typeparam>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    public static T FindComponentWithTag<T>(string tag) where T : Component {
+        return FindOneComponentWithTag<T>(tag);
+    }
+
+    /// <summary>
+    /// Find all GameObjects with a given tag. Designed to mimic the standard tag API but 
+    /// really just a wrapper for calling FindAnyGameObjectsWithTag(). 
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <returns></returns>
     public static GameObject[] FindGameObjectsWithTag(string tag) {
         return FindAllGameObjectsWithTag(tag);
     }
@@ -55,16 +74,55 @@ public class MultiTag : MonoBehaviour {
     /// <returns></returns>
     public static GameObject FindOneGameObjectWithTag(string tag) {
         if (tagMap.ContainsKey(tag)) {
-            return tagMap[tag][0];
+            return tagMap[tag].First();
         }
         return null;
     }
 
-    public static GameObject[] FindAllGameObjectsWithTag(string tag) {
-        if(tagMap.ContainsKey(tag)) {
-            return tagMap[tag].ToArray();
+    /// <summary>
+    /// Find a single Component on a GameObject with the given tag. What I feel is a far 
+    /// more sensible naming convention for the methods where they differ by more than a 
+    /// single 's' somewhere in the signature.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    public static T FindOneComponentWithTag<T>(string tag) where T : Component {
+        if (tagMap.ContainsKey(tag)) {
+            return tagMap[tag].First().GetComponent<T>();
         }
         return null;
+    }
+
+    /// <summary>
+    /// Find all GameObjects with a given tag. What I feel is a far more sensible naming 
+    /// convention for the methods where they differ by more than a single 's' somewhere in
+    /// the signature.
+    /// </summary>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    public static GameObject[] FindAllGameObjectsWithTag(string tag) {
+        if (tagMap.ContainsKey(tag)) {
+            return tagMap[tag].ToArray();
+        }
+        return new GameObject[] { };
+    }
+
+    /// <summary>
+    /// Find all Components of Type T on all GameObjects with a given tag. If a particular
+    /// GameObject does not have the desired type of Component then it will be excluded 
+    /// from the list.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    public static T[] FindAllComponentsWithTag<T>(string tag) where T : Component {
+        if (tagMap.ContainsKey(tag)) {
+            return (from GameObject go
+                    in tagMap[tag]
+                    select go.GetComponent<T>()).Where(c => c != null).ToArray();
+        }
+        return new T[] { };
     }
 
     /// <summary>
@@ -72,12 +130,27 @@ public class MultiTag : MonoBehaviour {
     /// </summary>
     /// <param name="tags"></param>
     /// <returns></returns>
-    public static GameObject[] FindAnyGameObjectsWithTags(string[] tags) {
+    public static GameObject[] FindAnyGameObjectsWithTags(params string[] tags) {
         HashSet<GameObject> ret = new HashSet<GameObject>();
         foreach (string t in tags) {
             ret.UnionWith(FindAllGameObjectsWithTag(t));
         }
         return ret.ToArray();
+    }
+
+    public static T[] FindAnyComponentsWithTags<T>(params string[] tags) where T : Component {
+        return (from GameObject go
+                in FindAnyGameObjectsWithTags(tags)
+                select go.GetComponent<T>()).Where(c => c != null).ToArray();
+    }
+
+    public static int CountGameObjectsWithTag(string tag) {
+        if (tagMap.ContainsKey(tag)) {
+            return tagMap.Count;
+        }
+        else {
+            return 0;
+        }
     }
 
 
@@ -100,7 +173,11 @@ public class MultiTag : MonoBehaviour {
     }
 
     [SerializeField()]
-    private  List<string> _multitags = new List<string>();
+    private List<string> _multitags = new List<string>();
+
+    public bool HasMultiTag(string tag) {
+        return _multitags.Contains(tag);
+    }
 
     public void AddMultiTag(string tag) {
         if (!_multitags.Contains(tag)) {
@@ -128,7 +205,7 @@ public class MultiTag : MonoBehaviour {
     public void RemoveMultiTags(string[] tags) {
         foreach (string s in tags) {
             _multitags.Remove(s);
-            RemoveFromMap(s,this.gameObject);
+            RemoveFromMap(s, this.gameObject);
         }
     }
 
